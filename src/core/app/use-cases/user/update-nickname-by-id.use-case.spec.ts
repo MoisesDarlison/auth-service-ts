cd4fd9af8c88ec56389c6c4a6017e6740051aa76
Darlison <moisesdarlison91@gmail.com>
2023-01-09 14:00:58 -0300

    chore: remove logs on terminal

--- src/core/app/use-cases/user/update-nickname-by-id.use-case.spec.ts
+++ src/core/app/use-cases/user/update-nickname-by-id.use-case.spec.ts
@@ -19,11 +19,7 @@ describe("Update User by ID - Use Case", () => {
     expect(userRepoInMemory.items).toHaveLength(1);
     expect(userRepoInMemory.items[0].updatedAt).toBeNull();
 
-    const output = await userUserCase.execute(
-      userRepoInMemory.items[0].id,
-      "John Due"
-    );
-    console.log(userRepoInMemory.items[0]);
+    await userUserCase.execute(userRepoInMemory.items[0].id, "John Due");
 
     expect(userRepoInMemory.items[0].nickName).toBe("John Due");
     expect(userRepoInMemory.items[0].createdAt).not.toBeNull();
@@ -31,5 +27,25 @@ describe("Update User by ID - Use Case", () => {
     expect(userRepoInMemory.items[0].createdAt).not.toEqual(
       userRepoInMemory.items[0].updatedAt
     );
+  });
+
+  it("Should be NOT able update mickName by Id - with invalid id", async () => {
+    const userUserCase = new UpdateNickNameByIdUserCase(userRepoInMemory);
+
+    userRepoInMemory.items.push(
+      new User({
+        email: "test@email.com",
+        password: "123456",
+        permissionLevel: "ADMIN" as Role,
+      })
+    );
+
+    expect(userRepoInMemory.items).toHaveLength(2);
+
+    const fakeID: string = "54447177-433c-4ddd-834f-49daa3e266eb";
+
+    expect(async () => {
+      return userUserCase.execute(fakeID, "John Due");
+    }).rejects.toThrow();
   });
 });
