56042b00eadd3d53dd99e0fa1ab3e7933f452ba1
Darlison <moisesdarlison91@gmail.com>
2023-01-10 12:16:56 -0300

    test: update file name in filter by email

--- src/core/app/use-cases/user/filter-user-by-email.use-case.spec.ts
+++ src/core/app/use-cases/user/filter-user-by-email.use-case.spec.ts
@@ -1,9 +1,9 @@
 import { Role, User } from "../../../domain/entities/User.entity";
 import { UserInMemoryRepository } from "../../../infra/db/in-memory/user.in-memory.repository";
-import { FilterUserByEmailUserCase } from "./filter-user-by-name.use-case";
+import { FilterUserByEmailUserCase } from "./filter-user-by-email.use-case";
 
 describe("Filter User by ID - Use Case", () => {
-  it("Should be able Filter user by Id ", async () => {
+  it("Should be able Filter user by Email", async () => {
     const userRepoInMemory = new UserInMemoryRepository();
     const userUserCase = new FilterUserByEmailUserCase(userRepoInMemory);
 
@@ -22,7 +22,7 @@ describe("Filter User by ID - Use Case", () => {
     expect(output?.email).toBe("test@email.com");
   });
 
-  it("Should to return NULL if it does not find the user by ID", async () => {
+  it("Should to return NULL if it does not find the user by EMAIL", async () => {
     const userRepoInMemory = new UserInMemoryRepository();
     const userUserCase = new FilterUserByEmailUserCase(userRepoInMemory);
 
